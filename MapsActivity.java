472221167338201fa9e5f1c4305fbf5d6612b992
sistemas <vmanriquec@crece.uss.edu.pe>
2017-09-24 23:21:45 -0500

    mapa resuelto

--- MapsActivity.java
+++ MapsActivity.java
@@ -2,67 +2,234 @@ package com.food.sistemas.sodapopapp;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.squareup.picasso.Picasso;
+import com.orhanobut.logger.Logger;
 
-import jp.wasabeef.picasso.transformations.CropCircleTransformation;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-    String session,nombreususrio,almacenactivo;
-    String FileName ="myfile";
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener,
+        View.OnClickListener {
+    String session, nombreususrio, almacenactivo;
+    String FileName = "myfile";
     private GoogleMap mMap;
     private PicassoMarker target;
+    public static final int LOCATION_UPDATE_MIN_DISTANCE = 10;
+    public static final int LOCATION_UPDATE_MIN_TIME = 5000;
+
+
+    private double longitude;
+    private double latitude;
+
+    private GoogleApiClient googleApiClient;
+
+    private GoogleMap mGoogleMap;
+    private LocationListener mLocationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                Logger.d(String.format("%f, %f", location.getLatitude(), location.getLongitude()));
+                drawMarker(location);
+                mLocationManager.removeUpdates((android.location.LocationListener) mLocationListener);
+            } else {
+                Logger.d("Location is null");
+            }
+        }
+
+
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+
+        public void onProviderEnabled(String s) {
+
+        }
+
+
+        public void onProviderDisabled(String s) {
+
+        }
+
+
+    };
+    private LocationManager mLocationManager;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {    super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        SharedPreferences sharedPreferences=getSharedPreferences(FileName, Context.MODE_PRIVATE);
-        session=sharedPreferences.getString("sessionid","");
+        SharedPreferences sharedPreferences = getSharedPreferences(FileName, Context.MODE_PRIVATE);
+        session = sharedPreferences.getString("sessionid", "");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        initMap();
+        getCurrentLocation();
+
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //     map.onDestroy();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //   mMapView.onResume();
+        getCurrentLocation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // mMapView.onPause();
+        mLocationManager.removeUpdates((android.location.LocationListener) mLocationListener);
+    }
+
+    private void initMap() {
+        int googlePlayStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (googlePlayStatus != ConnectionResult.SUCCESS) {
+            GooglePlayServicesUtil.getErrorDialog(googlePlayStatus, this, -1).show();
+            finish();
+        } else {
+            if (mGoogleMap != null) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mGoogleMap.setMyLocationEnabled(true);
+                mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                mGoogleMap.getUiSettings().setAllGesturesEnabled(true);
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location location = null;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+            Logger.d(String.format("getCurrentLocation(%f, %f)", location.getLatitude(),
+                    location.getLongitude()));
+            drawMarker(location);
+
+    }
+    private void drawMarker(Location location) {
+        if (mGoogleMap != null) {
+            mGoogleMap.clear();
+            LatLng gps = new LatLng(location.getLatitude(), location.getLongitude());
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(gps)
+                    .title("Current Position"));
+            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps, 12));
+        }
+
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //Creating a location object
 
-        LatLng sydney = new LatLng(-12.0167, -77.1167);
 /*        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         PicassoMarker marker = new PicassoMarker(mMap);
         Picasso.with(MapsActivity.this).load(icon_url).into(marker);
 */
 
-        String imgUrl = "https://graph.facebook.com/"+session+"/picture?type=large";
-        MarkerOptions markerOne = new MarkerOptions().position(sydney).title("hola");
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        target = new PicassoMarker(mMap.addMarker(markerOne));
-        Picasso.with(MapsActivity.this).load(imgUrl).transform(new CropCircleTransformation()).resize(50, 50).into(target);
+//        pintar();
+     getCurrentLocation();
+    }
+    public void pintar(){
 
     }
 
 
+    @Override
+    public void onClick(View view) {
 
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
 }
